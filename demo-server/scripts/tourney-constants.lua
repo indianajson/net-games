e507a96a1a12ac571e51c03fc0de896e720bba73
Ninjaman255 <aaronclevette@gmail.com>
2025-10-21 02:28:03 -0500

    Added BGs and BG anim to tourney-constants.lua.

---
+++ demo-server/scripts/tourney-constants.lua
@@ -0,0 +1,15 @@
+local CONSTANTS = {
+    bracket_background_path = {
+        blue_bn4 = "/server/assets/tourney/bgs/blue-bg.png",
+        green_bn4 = "/server/assets/tourney/bgs/green-bg.png",
+        orange_bn4 = "/server/assets/tourney/bgs/orange-bg.png",
+        purple_bn4 = "/server/assets/tourney/bgs/purple-bg.png",
+        blue_bn45 = "/server/assets/tourney/bgs/blue-checkered-bg.png",        
+        green_bn45 = "/server/assets/tourney/bgs/green-checkered-bg.png",
+        silver_bn45 = "/server/assets/tourney/bgs/silver-checkered-bg.png",        
+        yellow_bn45 = "/server/assets/tourney/bgs/yellow-checkered-bg.png",
+    },
+    bracket_background_anim_path = "/server/assets/tourney/bgs/bg.anim"
+}
+
+return CONSTANTS
