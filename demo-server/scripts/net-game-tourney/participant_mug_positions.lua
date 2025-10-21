27786696ec0fd442da2bf30a5f14547a65cb7635
Ninjaman255 <aaronclevette@gmail.com>
2025-10-21 17:51:15 -0500

    Reorganizing and seperating out constants/unchanging elements for easier updates based on incoming changes from upstream main branch.

---
+++ demo-server/scripts/net-game-tourney/participant_mug_positions.lua
@@ -0,0 +1,40 @@
+local POSITIONS = {
+top_tier = {},
+middle_tier_interprolation ={},
+middle_tier={},
+bottom_tier_interprolation ={},
+bottom_tier = {
+        [1] = {
+            x = -112,
+            y = -48,
+        },
+        [2] = {
+            x = -86,
+            y = -48,
+        },
+        [3] = {
+            x = -56,
+            y = -48,
+        },
+        [4] = {
+            x = -30,
+            y = -48,
+        },
+        [5] = {
+            x = 8,
+            y = -48,
+        },
+        [6] = {
+            x = 34,
+            y = -48,
+        },
+        [7] = {
+            x = 64,
+            y = -48,
+        },
+        [8] = {
+            x = 90,
+            y = -48,
+        },
+    }
+}
---
+++ demo-server/scripts/net-game-tourney/static_ui_data.lua
@@ -0,0 +1,51 @@
+local UI_DATA = {
+    frame_names = {
+    "MUG_FRAME_" .. 1,
+    "MUG_FRAME_" .. 2,
+    "MUG_FRAME_" .. 3,
+    "MUG_FRAME_" .. 4, 
+    "MUG_FRAME_" .. 5, 
+    "MUG_FRAME_" .. 6,
+    "MUG_FRAME_" .. 7, 
+    "MUG_FRAME_" .. 8,
+    "MUG_" .. 1, 
+    "MUG_" .. 2, 
+    "MUG_" .. 3,
+    "MUG_" .. 4, 
+    "MUG_" .. 5, 
+    "MUG_" .. 6,
+    "MUG_" .. 7, 
+    "MUG_" .. 8, 
+    "BOARD BG",
+    "BRACKET", 
+    "TITLE BANNER",
+    --"CROWN_1", 
+    --"CROWN_2",
+    },
+    unmoving_ui_pos = {
+        bg = {
+            x= 0,
+            y = 0,
+            z = -2,
+        },
+        grid={
+            x= 0,
+            y = 0,
+            z = -1,
+        },
+        title_banner = {
+            x= 0,
+            y = 0,
+            z = 0,
+        },
+        bracket = {
+            x= 0,
+            y = 0,
+            z = 0,
+        },
+        crown1 = {}, 
+        crown2 ={},
+    },
+}
+
+return UI_DATA
